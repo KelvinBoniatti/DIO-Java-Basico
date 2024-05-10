d584f52a2c7e746611bf87459aa9110e54e7f296
Kelvin_Boniatti <kelvinboniatti@outlook.com>
2024-05-09 21:24:53 -0300

    PrintIn meu nome

---
+++ JavaAnatomiaClasses/src/JavaAnatomiaClasses.java
@@ -0,0 +1,18 @@
+public class JavaAnatomiaClasses {
+
+public static void main (String [] args){ 
+
+    String primeiroNome = "Kelvin";
+    String segundoNome = "Boniatti";
+    String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+    /* Váriavel e método com mesmo nome, porém todo método possui parametros() assim que se diferencia*/
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método = " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+    
+
+}
