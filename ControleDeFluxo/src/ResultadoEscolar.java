17dfa72fed0003f7a44d7a1a04e3d8c3ed5bab04
Kelvin_Boniatti <kelvinboniatti@outlook.com>
2024-05-13 17:05:29 -0300

    Condição ternária

--- ControleDeFluxo/src/ResultadoEscolar.java
+++ ControleDeFluxo/src/ResultadoEscolar.java
@@ -1,16 +1,8 @@
 public class ResultadoEscolar {
     public static void main (String[] args){
-    //Condicional encadeada:
-        int nota = 2;
-
-    if (nota >= 7)
-        System.out.println("Aprovado");
-    
-    else if (nota >=5 && nota < 7) 
-        System.out.println("Recuperação");
-    
-    else 
-        System.out.println("Reprovado");
-                   
+    //Condição ternária:
+        int nota = 6;
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
  }
 }
